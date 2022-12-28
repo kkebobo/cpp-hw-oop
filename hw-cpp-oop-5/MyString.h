ea5554eb97be7450b0aaae89f18dcb8ba385383b
veronica <laurens.boychuk@gmail.com>
2022-12-28 18:23:43 +0100

    Create MyString.h

---
+++ hw-cpp-oop-5/MyString.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+class MyString
+{
+	char* str;
+	int l;
+public:
+	MyString();
+	MyString(int size);
+	MyString(const char* st);
+	~MyString();
+
+
+	void MyStrcpy(MyString& obj); //копирование строк
+	void Print();
+	bool MyStrStr(const char* str); 
+	int MyChr(char c); 
+	int MyStrLen();
+	void MyStrCat(MyString& b); 
+	void MyDelChr(char c);  
+	int MyStrCmp(MyString& b);
+};
+
